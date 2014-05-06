3fca7f50f9eaacc96c8be5e4411a06e6cace036d
frankie <fhuster@grameenfoundation.org>
2014-05-06 15:34:24 -0700

    simplifying to try to make things work
    Change-Id: Icd57507eeb64159c50515721724d8892264921bc

--- modules/ivr/api/src/main/java/org/motechproject/ivr/domain/CallDetailRecord.java
+++ modules/ivr/api/src/main/java/org/motechproject/ivr/domain/CallDetailRecord.java
@@ -5,11 +5,7 @@ import org.joda.time.Period;
 import org.motechproject.mds.annotations.Entity;
 import org.motechproject.mds.annotations.Field;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static org.motechproject.commons.date.util.DateUtil.newDateTime;
 import static org.motechproject.commons.date.util.DateUtil.now;
@@ -40,10 +36,6 @@ public class CallDetailRecord implements CallDetail {
     private Integer duration;
     @Field
     private CallDirection callDirection;
-
-    private List<CallEventLog> callEventLogs = new ArrayList<CallEventLog>();
-
-    private Map<String, Object> customProperties = new HashMap<>();
 
     private CallDetailRecord() {
     }
@@ -135,70 +127,37 @@ public class CallDetailRecord implements CallDetail {
         return callDirection;
     }
 
-    public List<CallEventLog> getCallEvents() {
-        return callEventLogs;
-    }
-
-    public CallDetailRecord setPhoneNumber(String phoneNumber) {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
-        return this;
     }
 
-    public CallDetailRecord setCallEvents(List<CallEventLog> callEventLogs) {
-        this.callEventLogs = callEventLogs;
-        return this;
-    }
-
-    public CallDetailRecord setCallDirection(CallDirection callDirection) {
+    public void setCallDirection(CallDirection callDirection) {
         this.callDirection = callDirection;
-        return this;
     }
 
-    public CallDetailRecord setStartDate(DateTime startDate) {
+    public void setStartDate(DateTime startDate) {
         this.startDate = startDate;
-        return this;
     }
 
-    public CallDetailRecord setEndDate(DateTime endDate) {
+    public void setEndDate(DateTime endDate) {
         this.endDate = endDate;
         duration = new Period(startDate, endDate).toStandardSeconds().getSeconds();
-        return this;
     }
 
-    public CallDetailRecord setAnswerDate(Date answerDate) {
+    public void setAnswerDate(Date answerDate) {
         this.answerDate = answerDate;
-        return this;
     }
 
-    public CallDetailRecord setDisposition(CallDisposition disposition) {
+    public void setDisposition(CallDisposition disposition) {
         this.disposition = disposition;
-        return this;
     }
 
-    public CallDetailRecord setErrorMessage(String errorMessage) {
+    public void setErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;
-        return this;
     }
 
-    public Map<String, Object> getCustomProperties() {
-        return customProperties;
-    }
-
-    public CallDetailRecord setCustomProperties(Map<String, Object> customProperties) {
-        if (customProperties != null) {
-            this.customProperties = customProperties;
-        }
-        return this;
-    }
-
-    public CallDetailRecord addCustomProperty(String key, Object value) {
-        this.customProperties.put(key, value);
-        return this;
-    }
-
-    public CallDetailRecord setCallId(String callId) {
+    public void setCallId(String callId) {
         this.callId = callId;
-        return this;
     }
 
     public Integer getDuration() {
@@ -208,24 +167,4 @@ public class CallDetailRecord implements CallDetail {
     public void setDuration(Integer duration) {
         this.duration = duration;
     }
-
-    /**
-     * Adds IVR events such as key press, hangup etc., to current call detail record.
-     *
-     * @param callEventLog
-     */
-    public CallDetailRecord addCallEvent(CallEventLog callEventLog) {
-        callEventLogs.add(callEventLog);
-        return this;
-    }
-
-    /**
-     * Get last call event for current call.
-     *
-     * @return Call event
-     */
-    public CallEventLog lastCallEvent() {
-        return callEventLogs.size() == 0 ? null : callEventLogs.get(callEventLogs.size() - 1);
-    }
-
 }
---
+++ modules/ivr/api/src/test/java/org/motechproject/ivr/it/CallDetailRecordServiceIT.java
@@ -0,0 +1,67 @@
+package org.motechproject.ivr.it;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.motechproject.ivr.domain.CallDetailRecord;
+import org.motechproject.ivr.domain.CallDirection;
+import org.motechproject.ivr.domain.CallDisposition;
+import org.motechproject.ivr.service.CallDetailRecordService;
+import org.motechproject.testing.osgi.BasePaxIT;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+import org.motechproject.testing.osgi.container.MotechNativeTestContainerFactory;
+import org.ops4j.pax.exam.ExamFactory;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerSuite;
+
+import javax.inject.Inject;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerSuite.class)
+@ExamFactory(MotechNativeTestContainerFactory.class)
+public class CallDetailRecordServiceIT extends BasePaxIT {
+    private static final String CALL_ID_A = "call-id-a";
+    private static final String CALL_ID_B = "call-id-b";
+    private static final String PHONE_NUMBER = "232";
+
+    @Inject
+    CallDetailRecordService callDetailRecordService;
+
+    @Before
+    public void setUp() throws Exception {
+        final CallDetailRecord callDetailRecordA = callDetailRecordService.create(
+                new CallDetailRecord(CALL_ID_A, PHONE_NUMBER));
+        PersistenceManager pm = JDOHelper.getPersistenceManager(callDetailRecordA);
+        final CallDetailRecord callDetailRecordB = callDetailRecordService.create(
+                new CallDetailRecord(CALL_ID_B, PHONE_NUMBER + "23"));
+        callDetailRecordB.setDisposition(CallDisposition.ANSWERED);
+        callDetailRecordB.setCallDirection(CallDirection.OUTBOUND);
+        callDetailRecordService.update(callDetailRecordB);
+    }
+
+    @Test
+    public void shouldSearchByCallId() throws Exception {
+        final List<CallDetailRecord> callDetailRecords = callDetailRecordService.findByCallId("a");
+        assertEquals("a", callDetailRecords.get(0).getCallId());
+    }
+
+    @After
+    public void tearDown() {
+        CallDetailRecord callDetailRecordA = callDetailRecordService.findByCallId(CALL_ID_A).get(0);
+        CallDetailRecord callDetailRecordB = callDetailRecordService.findByCallId(CALL_ID_B).get(0);
+        PersistenceManager pm = JDOHelper.getPersistenceManager(callDetailRecordA);
+        callDetailRecordService.delete(callDetailRecordA);
+        callDetailRecordService.delete(callDetailRecordB);
+    }
+}
