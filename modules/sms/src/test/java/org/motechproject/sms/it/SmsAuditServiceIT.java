cb23ea023f9ee2e204e10b936643f11d8a5a3fe8
frankie <fhuster@grameenfoundation.org>
2014-05-06 18:14:57 -0700

    still no luck running IT
    Change-Id: I320920cbb25082c68ce009aa459ee1bbb3c4836a

---
+++ modules/sms/src/test/java/org/motechproject/sms/it/SmsAuditServiceIT.java
@@ -0,0 +1,38 @@
+package org.motechproject.sms.it;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.motechproject.sms.audit.*;
+import org.motechproject.testing.osgi.BasePaxIT;
+import org.motechproject.testing.osgi.container.MotechNativeTestContainerFactory;
+import org.ops4j.pax.exam.ExamFactory;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerSuite;
+import javax.inject.Inject;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerSuite.class)
+@ExamFactory(MotechNativeTestContainerFactory.class)
+public class SmsAuditServiceIT  extends BasePaxIT {
+    @Inject
+    private SmsAuditService smsAuditService;
+
+    @Before
+    public void setUp() throws Exception {
+        smsAuditService.log(new SmsRecord("config", SmsDirection.OUTBOUND, "123", "msg", DateTime.now(),
+                DeliveryStatus.DELIVERY_CONFIRMED, null, null, null, null));
+    }
+
+    @Test
+    public void shouldFindAllRecords() throws Exception {
+
+        List<SmsRecord> smsRecords = smsAuditService.findAllSmsRecords();
+        assertTrue(smsRecords.size() >= 1);
+    }
+}
---
+++ modules/sms/src/test/java/org/motechproject/sms/it/SmsIntegrationTests.java
@@ -0,0 +1,8 @@
+package org.motechproject.sms.it;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({SmsAuditServiceIT.class})
+public class SmsIntegrationTests { }
